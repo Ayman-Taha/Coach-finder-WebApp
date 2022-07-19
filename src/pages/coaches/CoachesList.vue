f51ee5527ed36a00921c6abd93b487b952c25cdd
Ayman Mohamed <aymantaha632@gmail.com>
2022-07-18 22:54:30 -0400

    building the coaches list page
    - coach list component
    - add styling and dummy data

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,6 +5,45 @@
       <button>button</button>
       <router-link to="/register">Register</router-link>
     </div>
-    <ul></ul>
+    <ul v-if="hasCoaches">
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :firstName="coach.firstName"
+        :lastName="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      ></coach-item>
+    </ul>
+    <h3 v-else>No coaches found.</h3>
   </section>
 </template>
+
+<script>
+import coachItem from '../../components/coaches/coachItem.vue';
+export default {
+  components: { coachItem },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
