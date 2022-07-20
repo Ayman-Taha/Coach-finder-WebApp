88fcb851a0646515239a8098a9041aa7600af375
Ayman Mohamed <aymantaha632@gmail.com>
2022-07-20 00:00:26 -0400

    registering base card

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import App from './App.vue';
 import store from './store/index.js';
 import router from './router.js';
 
+import BaseCard from './components/ui/BaseCard.vue';
+
 const app = createApp(App);
 app.use(router);
 app.use(store);
+
+app.component('base-card', BaseCard);
+
 app.mount('#app');
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,22 +1,24 @@
 <template>
   <section></section>
   <section>
-    <div class="controls">
-      <button>button</button>
-      <router-link to="/register">Register</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :firstName="coach.firstName"
-        :lastName="coach.lastName"
-        :rate="coach.hourlyRate"
-        :areas="coach.areas"
-      ></coach-item>
-    </ul>
-    <h3 v-else>No coaches found.</h3>
+    <base-card>
+      <div class="controls">
+        <button>button</button>
+        <router-link to="/register">Register</router-link>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :firstName="coach.firstName"
+          :lastName="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 
