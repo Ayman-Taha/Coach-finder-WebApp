e5be5ae60b147c2f4ef8e02991623ee7e27d6e86
Ayman Mohamed <aymantaha632@gmail.com>
2022-07-20 22:51:03 -0400

    base badge
    adding, styling, registering and using base badge

--- src/components/coaches/coachItem.vue
+++ src/components/coaches/coachItem.vue
@@ -3,7 +3,12 @@
     <h3>{{ fullName }}</h3>
     <h4>${{ rate }}/hr</h4>
     <div>
-      <span v-for="area in areas" :key="area">{{ area }}</span>
+      <base-badge
+        v-for="area in areas"
+        :key="area"
+        :type="area"
+        :title="area"
+      ></base-badge>
     </div>
     <div class="actions">
       <base-button mode="outline" to="coachContactLink">Contact</base-button>
--- src/components/ui/BaseBadge.vue
+++ src/components/ui/BaseBadge.vue
@@ -1,4 +1,18 @@
-<template></template>
+<template>
+  <span class="badge" :class="type">{{ text }}</span>
+</template>
+
+<script>
+export default {
+  props: ['type', 'title'],
+  computed: {
+    text() {
+      return this.title.toUpperCase();
+    },
+  },
+};
+</script>
+
 <style scoped>
 .badge {
   background-color: #ccc;
