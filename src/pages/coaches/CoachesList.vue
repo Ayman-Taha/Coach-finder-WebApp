3bed5ad3505d14f5ab418bf9e3c81d21220a0cc2
Ayman Mohamed <aymantaha632@gmail.com>
2022-07-29 00:34:21 -0400

    hiding register button after registering

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,7 +6,9 @@
     <base-card>
       <div class="controls">
         <base-button mode="outline">Refresh</base-button>
-        <base-button :link="true" to="register">Register</base-button>
+        <base-button v-if="!isCoach" :link="true" to="register"
+          >Register</base-button
+        >
       </div>
       <ul v-if="hasCoaches">
         <coach-item
@@ -63,6 +65,9 @@ export default {
     },
     hasCoaches() {
       return this.$store.getters['coaches/hasCoaches'];
+    },
+    isCoach() {
+      return this.$store.getters['coaches/isCoach'];
     },
   },
 };
