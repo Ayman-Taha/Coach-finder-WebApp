12ed0494989d4ff4bf36a0d0e1b145c4d8e77305
Ayman Mohamed <aymantaha632@gmail.com>
2022-07-29 23:28:08 -0400

    Adding contact form

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,1 +1,15 @@
-<template>contact</template>
+<template>
+  <form @submit.prevent="submitForm">
+    <div class="form-control">
+      <label for="email">Enter your email:</label>
+      <input type="email" id="email" />
+    </div>
+    <div class="form-control">
+      <label for="message">Enter your message:</label>
+      <textarea rows="6" id="message"></textarea>
+    </div>
+    <div class="actions">
+      <base-button>Send Message</base-button>
+    </div>
+  </form>
+</template>
