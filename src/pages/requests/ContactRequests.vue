f58350cf525ad723a7a7d79873c9aff31d389471
Ayman Mohamed <aymantaha632@gmail.com>
2022-08-01 23:53:58 -0400

    implementing requests page
    template, style, connect to store

--- src/pages/requests/ContactRequests.vue
+++ src/pages/requests/ContactRequests.vue
@@ -1,1 +1,38 @@
-<template>Requests</template>
+<template>
+  <section>
+    <base-card>
+      <header><h2>Requests Received</h2></header>
+      <ul v-if="hasRequests"></ul>
+      <h3 v-else>You don't have requests yet.</h3></base-card
+    >
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    receivedRequests() {
+      return this.$stire.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$stire.getters['requests/hasRequests'];
+    },
+  },
+};
+</script>
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
