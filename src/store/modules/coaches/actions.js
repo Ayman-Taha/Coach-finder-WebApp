5f1cea4a9f6a9ea8687c87cd67b84235adccbd80
Ayman Mohamed <aymantaha632@gmail.com>
2022-08-03 00:00:13 -0400

    adding firebase  db

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -8,6 +8,10 @@ export default {
       hourlyRate: data.rate,
       areas: data.areas,
     };
+
+    fetch(
+      'https://console.firebase.google.com/project/vue-find-coach-23a5c/database/vue-find-coach-23a5c-default-rtdb/data/~2F'
+    );
     context.commit('registerCoach', coachData);
   },
 };
