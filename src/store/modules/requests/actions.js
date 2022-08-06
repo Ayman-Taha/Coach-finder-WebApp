7cf61c7cef7a66baaceb1c482f13af02f72ae272
Ayman Mohamed <aymantaha632@gmail.com>
2022-08-05 23:59:34 -0400

    request db

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,16 @@
 export default {
-  contactCoach(context, data) {
+  async contactCoach(context, data) {
     const request = {
-      requestId: new Date().toISOString,
       coachId: data.coachId,
       email: data.email,
       message: data.message,
     };
+
+    const res = await fetch(
+      `https://vue-find-coach-23a5c-default-rtdb.firebaseio.com/requests/${data.coachId}.json`,
+      { method: 'POST', body: JSON.stringify(request) }
+    );
+
     context.commit('addRequest', request);
   },
 };
