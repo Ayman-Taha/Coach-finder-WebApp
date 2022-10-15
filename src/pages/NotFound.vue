d6bdda84d85bd53f8f3354e6f5f96f3d672d875b
Ayman Mohamed <aymantaha632@gmail.com>
2022-10-15 01:01:57 -0400

    Update NotFound.vue

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,1 +1,11 @@
-<template>not found</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Page is not found</h2>
+      <p>
+        This page couldn't be found, please check our
+        <router-link to="/coaches">coaches</router-link>.
+      </p>
+    </base-card>
+  </section>
+</template>
